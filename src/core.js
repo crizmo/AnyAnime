6bb0852a600d66869d00fdeca9504371f4ace4a0
Criz <crizmo595@gmail.com>
2022-03-16 22:52:59 +0530

    Update core.js

--- src/core.js
+++ src/core.js
@@ -13,7 +13,7 @@ const AnyAnime = () => {
                 console.log('\x1b[32m' + '---------------------------------------------------')
                 console.log('\x1b[33m' + `|            The module is\x1b[31m out of date !\x1b[33m          |`)
                 console.log('\x1b[35m' + '|             New version is available!           |')
-                console.log('\x1b[34m' + `|                   ${require('../package.json').version} --> ${packageData['dist-tags'].latest}               |`)
+                console.log('\x1b[34m' + `|                   ${require('../package.json').version} --> ${packageData['dist-tags'].latest}              |`)
                 console.log('\x1b[36m' + '|             Run "npm i anyanime@latest"         |')
                 console.log('\x1b[36m' + '|                    to update!                   |')
                 console.log('\x1b[32m' + '---------------------------------------------------\x1b[37m')
